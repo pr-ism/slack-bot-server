6f1a1facac23d9e889ff6787ac0ce25b6e17e211
apptie <appti@naver.com>
2026-01-16 22:49:25 +0900

    test: 통합 테스트 기본 설정 추가

---
+++ src/test/java/com/slack/bot/context/exception/TestContextClassNotFoundException.java
@@ -0,0 +1,8 @@
+package com.slack.bot.context.exception;
+
+public class TestContextClassNotFoundException extends RuntimeException {
+
+    public TestContextClassNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
