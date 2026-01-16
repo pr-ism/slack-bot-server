7406a99a105c6a521e310b7ed1b4d52a49869a81
apptie <appti@naver.com>
2026-01-16 22:21:01 +0900

    fix: ErrorCode에 HttpStatus도 같이 관리하도록 변경
    - 관련 코드 변경

--- src/main/java/com/slack/bot/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/slack/bot/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.slack.bot.global.exception;
 
 import com.slack.bot.global.exception.dto.response.DefaultErrorCode;
+import com.slack.bot.global.exception.dto.response.ErrorCode;
 import com.slack.bot.global.exception.dto.response.ExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,9 +17,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ExceptionResponse> handleException(Exception ex) {
         log.error("Exception : ", ex);
 
-        ExceptionResponse response = ExceptionResponse.from(DefaultErrorCode.UNKNOWN_SERVER_EXCEPTION);
+        return createResponseEntity(DefaultErrorCode.UNKNOWN_SERVER_EXCEPTION);
+    }
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+    private ResponseEntity<ExceptionResponse> createResponseEntity(ErrorCode errorCode) {
+        ExceptionResponse response = ExceptionResponse.from(errorCode);
+
+        return ResponseEntity.status(errorCode.getHttpStatus())
                              .body(response);
     }
 }
--- src/main/java/com/slack/bot/global/exception/dto/response/DefaultErrorCode.java
+++ src/main/java/com/slack/bot/global/exception/dto/response/DefaultErrorCode.java
@@ -1,18 +1,21 @@
 package com.slack.bot.global.exception.dto.response;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum DefaultErrorCode implements ErrorCode {
 
-    UNKNOWN_SERVER_EXCEPTION("서버 에러");
+    UNKNOWN_SERVER_EXCEPTION("서버 에러", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String message;
+    private final HttpStatus httpStatus;
 
-    DefaultErrorCode(String message) {
+    DefaultErrorCode(String message, HttpStatus httpStatus) {
         this.message = message;
+        this.httpStatus = httpStatus;
     }
-    
+
     @Override
     public String getErrorCode() {
         return this.name();
