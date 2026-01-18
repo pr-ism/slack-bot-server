914cd76bb617b091de02e6dd45f8732ef05c730a
apptie <appti@naver.com>
2026-01-18 13:46:47 +0900

    feat: 공통 도메인 엔티티 추가

---
+++ src/main/java/com/slack/bot/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.slack.bot.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
