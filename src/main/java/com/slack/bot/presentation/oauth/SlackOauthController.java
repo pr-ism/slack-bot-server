61522ddf45e5c4c404b76e994b13918ee08b0734
apptie <appti@naver.com>
2026-01-21 10:01:55 +0900

    refactor: api 서버를 도메인으로 구분하도록 변경

--- src/main/java/com/slack/bot/presentation/oauth/SlackOauthController.java
+++ src/main/java/com/slack/bot/presentation/oauth/SlackOauthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("/api/slack")
+@RequestMapping("/slack")
 @RequiredArgsConstructor
 public class SlackOauthController {
 
