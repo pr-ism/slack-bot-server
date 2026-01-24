b46a037259301de2eb06398b72c1e64b0cc5fd1f
apptie <appti@naver.com>
2026-01-25 00:37:10 +0900

    fix: ConstraintViolationException 체크 범위를 좁혀 중복 키만 정확하게 판별하도록 변경

--- src/main/java/com/slack/bot/infrastructure/common/MysqlDuplicateKeyDetector.java
+++ src/main/java/com/slack/bot/infrastructure/common/MysqlDuplicateKeyDetector.java
@@ -10,23 +10,29 @@ public class MysqlDuplicateKeyDetector {
     public boolean isDuplicateKey(Throwable throwable) {
         Throwable current = throwable;
         while (current != null) {
-            if (current instanceof ConstraintViolationException) {
+            if (current instanceof ConstraintViolationException cve && isMysqlDuplicate(cve.getSQLException())) {
                 return true;
             }
-            if (current instanceof SQLException sqlException) {
-                String sqlState = sqlException.getSQLState();
-
-                // MySQL 무결성 제약 조건 코드
-                if ("23000".equals(sqlState) && sqlException.getErrorCode() == 1062) {
-                    return true;
-                }
+            if (current instanceof SQLException sqlException && isMysqlDuplicate(sqlException)) {
+                return true;
             }
+
             current = current.getCause();
         }
+
         return false;
     }
 
     public boolean isNotDuplicateKey(Throwable throwable) {
         return !isDuplicateKey(throwable);
+    }
+
+    private boolean isMysqlDuplicate(SQLException sqlException) {
+        if (sqlException == null) {
+            return false;
+        }
+
+        // MySQL 무결성 제약 조건 코드
+        return "23000".equals(sqlException.getSQLState()) && sqlException.getErrorCode() == 1062;
     }
 }
