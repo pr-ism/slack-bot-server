b032eddf4736848bfd4bffa4b33f06f6ba0c6554
apptie <appti@naver.com>
2026-01-24 17:46:18 +0900

    feat: 프로젝트 멤버 조회 기능 추가

---
+++ src/test/java/com/slack/bot/application/command/ProjectMemberReaderTest.java
@@ -0,0 +1,37 @@
+package com.slack.bot.application.command;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.slack.bot.application.IntegrationTest;
+import com.slack.bot.domain.member.ProjectMember;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+
+@IntegrationTest
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ProjectMemberReaderTest {
+
+    @Autowired
+    ProjectMemberReader projectMemberReader;
+
+    @Test
+    @Sql(scripts = "classpath:sql/fixtures/member/project_member_t1_u1.sql")
+    void 프로젝트_멤버를_조회한다() {
+        // given
+        // when
+        Optional<ProjectMember> actualMember = projectMemberReader.read("T1", "U1");
+
+        // then
+        assertAll(
+                () -> assertThat(actualMember).isPresent(),
+                () -> assertThat(actualMember.get().getSlackUserId()).isEqualTo("U1"),
+                () -> assertThat(actualMember.get().getTeamId()).isEqualTo("T1")
+        );
+    }
+}
