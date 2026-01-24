1ad4ae08a78ea40b4ad368d812f52abe15886bdf
apptie <appti@naver.com>
2026-01-25 00:43:07 +0900

    test: 로컬 영역 테스트에서 H2 기반으로 중복 키를 판단하도록 변경

--- src/test/java/com/slack/bot/application/IntegrationTestConfig.java
+++ src/test/java/com/slack/bot/application/IntegrationTestConfig.java
@@ -1,6 +1,9 @@
 package com.slack.bot.application;
 
 import com.slack.bot.application.command.client.MemberConnectionSlackApiClient;
+import com.slack.bot.infrastructure.common.MysqlDuplicateKeyDetector;
+import java.sql.SQLException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -22,6 +25,44 @@ public class IntegrationTestConfig {
             @Override
             public String resolveUserName(String token, String slackUserId) {
                 return "신규 사용자";
+            }
+        };
+    }
+
+    @Bean
+    @Primary
+    public MysqlDuplicateKeyDetector mysqlDuplicateKeyDetector() {
+        return new MysqlDuplicateKeyDetector() {
+
+            @Override
+            public boolean isDuplicateKey(Throwable throwable) {
+                if (isH2DuplicateKey(throwable)) {
+                    return true;
+                }
+                return super.isDuplicateKey(throwable);
+            }
+
+            private boolean isH2DuplicateKey(Throwable throwable) {
+                Throwable current = throwable;
+                while (current != null) {
+                    if (current instanceof ConstraintViolationException cve && isH2SqlState(cve.getSQLException())) {
+                        return true;
+                    }
+
+                    if (current instanceof SQLException sqlException && isH2SqlState(sqlException)) {
+                        return true;
+                    }
+
+                    current = current.getCause();
+                }
+                return false;
+            }
+
+            private boolean isH2SqlState(SQLException sqlException) {
+                if (sqlException == null) {
+                    return false;
+                }
+                return "23505".equals(sqlException.getSQLState());
             }
         };
     }
