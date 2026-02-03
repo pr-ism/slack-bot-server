8770e5075d4494a1e6e704190f07462938bfde05
apptie <appti@naver.com>
2026-02-03 15:00:16 +0900

    test: 테스트 fixture sql 추가

---
+++ src/test/resources/sql/fixtures/notification/notification_settings_t1_u1_dm_enabled.sql
@@ -0,0 +1,2 @@
+INSERT INTO notification_settings (id, project_member_id, delivery_space, reservation_canceled_confirmation_enabled, review_reminder_enabled, pull_request_mention_enabled, review_completed_enabled)
+VALUES (1, 1, 'DIRECT_MESSAGE', TRUE, TRUE, TRUE, TRUE);
