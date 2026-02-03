a13aef708c440cea0a418378d9198137fd6655e8
apptie <appti@naver.com>
2026-02-04 00:18:01 +0900

    feat: ReminderScheduleCommandDto 빌더 패턴 적용

--- src/main/java/com/slack/bot/application/interactivity/reminder/dto/ReminderScheduleCommandDto.java
+++ src/main/java/com/slack/bot/application/interactivity/reminder/dto/ReminderScheduleCommandDto.java
@@ -1,11 +1,15 @@
 package com.slack.bot.application.interactivity.reminder.dto;
 
+import com.slack.bot.application.interactivity.reservation.dto.ReservationCommandDto;
 import com.slack.bot.domain.reservation.ReviewReminder;
+import com.slack.bot.domain.reservation.ReviewReservation;
 import com.slack.bot.domain.reservation.vo.ReminderDestination;
 import com.slack.bot.domain.reservation.vo.ReminderParticipants;
 import com.slack.bot.domain.reservation.vo.ReminderPullRequest;
 import java.time.Instant;
+import lombok.Builder;
 
+@Builder
 public record ReminderScheduleCommandDto(
         Long reservationId,
         Instant scheduledAt,
@@ -16,6 +20,18 @@ public record ReminderScheduleCommandDto(
         String pullRequestUrl,
         String pullRequestTitle
 ) {
+
+    public static ReminderScheduleCommandDto from(ReviewReservation reservation, ReservationCommandDto command) {
+        return builder().reservationId(reservation.getId())
+                        .scheduledAt(command.scheduledAt())
+                        .teamId(command.teamId())
+                        .channelId(command.channelId())
+                        .pullRequestAuthorSlackId(command.authorSlackId())
+                        .reviewerSlackId(command.reviewerSlackId())
+                        .pullRequestUrl(command.reservationPullRequest().getPullRequestUrl())
+                        .pullRequestTitle(command.reservationPullRequest().getPullRequestTitle())
+                        .build();
+    }
 
     public ReviewReminder toReminder() {
         return ReviewReminder.builder()
