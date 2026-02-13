58b24b066cc8524dcaef62192d3b9812e009d4de
apptie <appti@naver.com>
2026-02-13 21:20:21 +0900

    test: 추가된 도메인 엔티티 관련 테스트 설정 추가

--- src/test/resources/sql/cleanup.sql
+++ src/test/resources/sql/cleanup.sql
@@ -1,6 +1,7 @@
 SET REFERENTIAL_INTEGRITY FALSE;
 
 TRUNCATE TABLE review_reminders;
+TRUNCATE TABLE review_reservation_interactions;
 TRUNCATE TABLE review_reservations;
 TRUNCATE TABLE workspaces;
 TRUNCATE TABLE oauth_verification_states;
