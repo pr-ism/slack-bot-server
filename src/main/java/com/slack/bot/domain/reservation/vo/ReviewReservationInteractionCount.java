4125fcc2a4803c8fb015627ec5bd76f5d0e2a8a9
apptie <appti@naver.com>
2026-02-13 20:40:10 +0900

    feat: 리뷰 예약 변경, 취소 횟수 관리 VO 추가

---
+++ src/main/java/com/slack/bot/domain/reservation/vo/ReviewReservationInteractionCount.java
@@ -0,0 +1,53 @@
+package com.slack.bot.domain.reservation.vo;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewReservationInteractionCount {
+
+    private int scheduleCancelCount;
+    private int scheduleChangeCount;
+
+    public static ReviewReservationInteractionCount defaults() {
+        return create(0, 0);
+    }
+
+    public static ReviewReservationInteractionCount create(int scheduleCancelCount, int scheduleChangeCount) {
+        validateScheduleCancelCount(scheduleCancelCount);
+        validateScheduleChangeCount(scheduleChangeCount);
+
+        return new ReviewReservationInteractionCount(scheduleCancelCount, scheduleChangeCount);
+    }
+
+    private ReviewReservationInteractionCount(int scheduleCancelCount, int scheduleChangeCount) {
+        this.scheduleCancelCount = scheduleCancelCount;
+        this.scheduleChangeCount = scheduleChangeCount;
+    }
+
+    public void increaseScheduleCancelCount() {
+        this.scheduleCancelCount = this.scheduleCancelCount + 1;
+    }
+
+    public void increaseScheduleChangeCount() {
+        this.scheduleChangeCount = this.scheduleChangeCount + 1;
+    }
+
+    private static void validateScheduleCancelCount(int scheduleCancelCount) {
+        if (scheduleCancelCount < 0) {
+            throw new IllegalArgumentException("예약 취소 횟수는 음수일 수 없습니다.");
+        }
+    }
+
+    private static void validateScheduleChangeCount(int scheduleChangeCount) {
+        if (scheduleChangeCount < 0) {
+            throw new IllegalArgumentException("예약 변경 횟수는 음수일 수 없습니다.");
+        }
+    }
+}
