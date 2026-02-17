dfb3981187f5072b47970e3018f3fd22abaa9c31
apptie <appti@naver.com>
2026-02-17 14:58:16 +0900

    feat: 슬랙 인터랙션 inbox 처리를 위한 백그라운드 워커 구현

---
+++ src/main/java/com/slack/bot/application/interactivity/box/in/SlackInteractionInboxWorker.java
@@ -0,0 +1,50 @@
+package com.slack.bot.application.interactivity.box.in;
+
+import com.slack.bot.global.config.properties.InteractivityWorkerProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SlackInteractionInboxWorker {
+
+    private static final int DEFAULT_BATCH_SIZE = 30;
+
+    private final SlackInteractionInboxProcessor slackInteractionInboxProcessor;
+    private final InteractivityWorkerProperties interactivityWorkerProperties;
+
+    @Scheduled(fixedDelayString = "${app.interactivity.inbox.block-actions.poll-delay-ms:200}")
+    public void processBlockActionInbox() {
+        if (isWorkerDisabled()) {
+            return;
+        }
+
+        try {
+            slackInteractionInboxProcessor.processPendingBlockActions(DEFAULT_BATCH_SIZE);
+        } catch (Exception e) {
+            log.error("block_actions inbox worker 실행에 실패했습니다.", e);
+        }
+    }
+
+    @Scheduled(fixedDelayString = "${app.interactivity.inbox.view-submission.poll-delay-ms:200}")
+    public void processViewSubmissionInbox() {
+        if (isWorkerDisabled()) {
+            return;
+        }
+
+        try {
+            slackInteractionInboxProcessor.processPendingViewSubmissions(DEFAULT_BATCH_SIZE);
+        } catch (Exception e) {
+            log.error("view_submission inbox worker 실행에 실패했습니다.", e);
+        }
+    }
+
+    private boolean isWorkerDisabled() {
+        return !interactivityWorkerProperties.inbox()
+                                             .blockActions()
+                                             .workerEnabled();
+    }
+}
