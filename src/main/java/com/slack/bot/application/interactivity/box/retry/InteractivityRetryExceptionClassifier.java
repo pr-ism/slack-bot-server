8cb5021e1c426a185a9ed62586f43c9f1b7d2742
apptie <appti@naver.com>
2026-02-17 13:53:36 +0900

    feat: 슬랙 인터랙션 시 재시도 대상 예외 관리 컴포넌트 추가

---
+++ src/main/java/com/slack/bot/application/interactivity/box/retry/InteractivityRetryExceptionClassifier.java
@@ -0,0 +1,47 @@
+package com.slack.bot.application.interactivity.box.retry;
+
+import com.slack.bot.application.interactivity.client.exception.SlackBotMessageDispatchException;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.web.client.ResourceAccessException;
+
+@Getter
+public class InteractivityRetryExceptionClassifier {
+
+    private final Map<Class<? extends Throwable>, Boolean> retryableExceptions;
+
+    public static InteractivityRetryExceptionClassifier create() {
+        Map<Class<? extends Throwable>, Boolean> retryableExceptions = createRetryableExceptions();
+
+        return new InteractivityRetryExceptionClassifier(retryableExceptions);
+    }
+
+    private static Map<Class<? extends Throwable>, Boolean> createRetryableExceptions() {
+        Map<Class<? extends Throwable>, Boolean> retryables = new HashMap<>();
+
+        retryables.put(SlackBotMessageDispatchException.class, true);
+        retryables.put(ResourceAccessException.class, true);
+        return Map.copyOf(retryables);
+    }
+
+    private InteractivityRetryExceptionClassifier(Map<Class<? extends Throwable>, Boolean> retryableExceptions) {
+        this.retryableExceptions = retryableExceptions;
+    }
+
+    public boolean isRetryable(Throwable throwable) {
+        Throwable cursor = throwable;
+
+        while (cursor != null) {
+            for (Class<? extends Throwable> retryableType : retryableExceptions.keySet()) {
+                if (retryableType.isAssignableFrom(cursor.getClass())) {
+                    return true;
+                }
+            }
+
+            cursor = cursor.getCause();
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/com/slack/bot/application/interactivity/box/retry/InteractivityRetryExceptionClassifierTest.java
@@ -0,0 +1,49 @@
+package com.slack.bot.application.interactivity.box.retry;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.slack.bot.application.interactivity.client.exception.SlackBotMessageDispatchException;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.ResourceAccessException;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class InteractivityRetryExceptionClassifierTest {
+
+    @Test
+    void 슬랙_전송_예외는_retryable로_판단한다() {
+        // given
+        InteractivityRetryExceptionClassifier exceptionClassifier = InteractivityRetryExceptionClassifier.create();
+
+        // when
+        boolean actual = exceptionClassifier.isRetryable(new SlackBotMessageDispatchException("temporary"));
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void 네트워크_접근_예외는_retryable로_판단한다() {
+        // given
+        InteractivityRetryExceptionClassifier exceptionClassifier = InteractivityRetryExceptionClassifier.create();
+
+        // when
+        boolean actual = exceptionClassifier.isRetryable(new ResourceAccessException("timeout"));
+
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void 일반_비즈니스_예외는_non_retryable로_판단한다() {
+        // given
+        InteractivityRetryExceptionClassifier exceptionClassifier = InteractivityRetryExceptionClassifier.create();
+
+        // when
+        boolean actual = exceptionClassifier.isRetryable(new IllegalArgumentException("invalid"));
+
+        assertThat(actual).isFalse();
+    }
+}
