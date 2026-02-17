38c686c3c7e674c73bec292ba83f614588922626
apptie <appti@naver.com>
2026-02-17 19:15:03 +0900

    feat: BlockActionInboxEnqueueAspect 로깅 추가

--- src/main/java/com/slack/bot/application/interactivity/box/aop/aspect/BlockActionInboxEnqueueAspect.java
+++ src/main/java/com/slack/bot/application/interactivity/box/aop/aspect/BlockActionInboxEnqueueAspect.java
@@ -5,11 +5,13 @@ import com.slack.bot.application.interactivity.box.aop.exception.BlockActionAopP
 import com.slack.bot.application.interactivity.box.in.SlackInteractionInboxProcessor;
 import com.slack.bot.application.interactivity.box.out.OutboxIdempotencySourceContext;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
@@ -24,7 +26,23 @@ public class BlockActionInboxEnqueueAspect {
             return proceedInInboxContext(joinPoint);
         }
 
-        slackInteractionInboxProcessor.enqueueBlockAction(payload.toString());
+        String payloadJson = payload.toString();
+        String payloadType = payload.path("type").asText(null);
+
+        try {
+            boolean enqueued = slackInteractionInboxProcessor.enqueueBlockAction(payloadJson);
+            if (!enqueued) {
+                log.info("block action enqueue가 중복 요청으로 스킵되었습니다. payloadType={}", payloadType);
+            }
+        } catch (RuntimeException runtimeException) {
+            log.error(
+                    "block action enqueue 처리 중 예외가 발생했습니다. payloadType={}",
+                    payloadType,
+                    runtimeException
+            );
+            throw runtimeException;
+        }
+
         return null;
     }
 
