9371874d2efc86cc239508337f4d509f559ad2af
apptie <appti@naver.com>
2026-02-21 21:21:05 +0900

    chore: SlackAppender 설정 변경

--- src/main/java/com/slack/bot/global/config/LogConfig.java
+++ src/main/java/com/slack/bot/global/config/LogConfig.java
@@ -1,18 +1,22 @@
 package com.slack.bot.global.config;
 
+import static org.slf4j.Logger.ROOT_LOGGER_NAME;
+
 import ch.qos.logback.classic.AsyncAppender;
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.filter.ThresholdFilter;
 import com.slack.bot.global.log.SlackAppender;
 import java.time.Clock;
+import java.time.Duration;
 import java.time.format.DateTimeFormatter;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Profile("slack-error-logging")
@@ -25,7 +29,13 @@ public class LogConfig {
 
     @Bean
     public RestClient slackWebhookRestClient() {
-        return RestClient.create();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(3));
+        factory.setReadTimeout(Duration.ofSeconds(5));
+
+        return RestClient.builder()
+                         .requestFactory(factory)
+                         .build();
     }
 
     @Bean
@@ -50,8 +60,13 @@ public class LogConfig {
     }
 
     private void registerSlackAppender(SlackAppender slackAppender) {
-        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        var rootLogger = context.getLogger(Logger.ROOT_LOGGER_NAME);
+        Object loggerFactory = LoggerFactory.getILoggerFactory();
+        if (!(loggerFactory instanceof LoggerContext context)) {
+            LoggerFactory.getLogger(LogConfig.class)
+                         .error("ILoggerFactory가 LoggerContext가 아닙니다. SlackAppender 등록을 건너뜁니다.");
+            return;
+        }
+        Logger rootLogger = context.getLogger(ROOT_LOGGER_NAME);
 
         if (rootLogger.getAppender(ASYNC_SLACK_APPENDER_NAME) != null) {
             rootLogger.detachAppender(ASYNC_SLACK_APPENDER_NAME);
