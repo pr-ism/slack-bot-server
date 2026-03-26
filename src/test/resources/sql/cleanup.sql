e71027f048a7e2188b5eb2ce2040af004bbdca20
apptie <appti@naver.com>
2026-03-27 02:05:44 +0900

    feat: ReviewNotificationOutboxHistory 엔티티 추가

--- src/test/resources/sql/cleanup.sql
+++ src/test/resources/sql/cleanup.sql
@@ -3,6 +3,7 @@ SET REFERENTIAL_INTEGRITY FALSE;
 TRUNCATE TABLE review_reminders;
 TRUNCATE TABLE slack_interaction_inbox;
 TRUNCATE TABLE slack_notification_outbox;
+TRUNCATE TABLE review_notification_outbox_history;
 TRUNCATE TABLE review_notification_outbox;
 TRUNCATE TABLE review_request_inbox_history;
 TRUNCATE TABLE review_request_inbox;
