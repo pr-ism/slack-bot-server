722a8b61d8afc6c4e1268cb293780dadbd10adb8
apptie <appti@naver.com>
2026-04-18 14:31:25 +0900

    test: slack interaction outbox JDBC fixture 상태 컬럼 반영

--- src/test/java/com/slack/bot/support/SlackNotificationOutboxJdbcFixture.java
+++ src/test/java/com/slack/bot/support/SlackNotificationOutboxJdbcFixture.java
@@ -1,17 +1,20 @@
 package com.slack.bot.support;
 
-import com.slack.bot.infrastructure.common.BoxEventTime;
-import com.slack.bot.infrastructure.common.BoxFailureSnapshot;
-import com.slack.bot.infrastructure.common.BoxProcessingLease;
+import com.slack.bot.infrastructure.common.BoxEventTimeState;
+import com.slack.bot.infrastructure.common.BoxFailureState;
+import com.slack.bot.infrastructure.common.BoxProcessingLeaseState;
 import com.slack.bot.infrastructure.interaction.box.SlackInteractionFailureType;
 import com.slack.bot.infrastructure.interaction.box.out.SlackNotificationOutbox;
 import com.slack.bot.infrastructure.interaction.box.out.SlackNotificationOutboxFieldState;
 import com.slack.bot.infrastructure.interaction.box.out.SlackNotificationOutboxHistory;
 import com.slack.bot.infrastructure.interaction.box.out.SlackNotificationOutboxMessageType;
 import com.slack.bot.infrastructure.interaction.box.out.SlackNotificationOutboxStatus;
-import com.slack.bot.infrastructure.interaction.box.out.SlackNotificationOutboxStringField;
+import com.slack.bot.infrastructure.interaction.box.persistence.out.SlackNotificationOutboxHistoryRow;
+import com.slack.bot.infrastructure.interaction.box.persistence.out.SlackNotificationOutboxRow;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -42,9 +45,13 @@ public final class SlackNotificationOutboxJdbcFixture {
                        fallback_text,
                        status,
                        processing_attempt,
+                       processing_lease_state,
                        processing_started_at,
+                       sent_time_state,
                        sent_at,
+                       failed_time_state,
                        failed_at,
+                       failure_state,
                        failure_reason,
                        failure_type
                 FROM slack_notification_outbox
@@ -64,6 +71,7 @@ public final class SlackNotificationOutboxJdbcFixture {
                        processing_attempt,
                        status,
                        completed_at,
+                       failure_state,
                        failure_reason,
                        failure_type
                 FROM slack_notification_outbox_history
@@ -83,6 +91,7 @@ public final class SlackNotificationOutboxJdbcFixture {
                        processing_attempt,
                        status,
                        completed_at,
+                       failure_state,
                        failure_reason,
                        failure_type
                 FROM slack_notification_outbox_history
@@ -95,97 +104,129 @@ public final class SlackNotificationOutboxJdbcFixture {
     }
 
     private SlackNotificationOutbox mapOutbox(ResultSet resultSet) throws SQLException {
-        return SlackNotificationOutbox.rehydrate(
-                resultSet.getLong("id"),
-                SlackNotificationOutboxMessageType.valueOf(resultSet.getString("message_type")),
-                resultSet.getString("idempotency_key"),
-                resultSet.getString("team_id"),
-                resultSet.getString("channel_id"),
-                toStringField(
-                        SlackNotificationOutboxFieldState.valueOf(resultSet.getString("user_id_state")),
-                        resultSet.getString("user_id")
-                ),
-                toStringField(
-                        SlackNotificationOutboxFieldState.valueOf(resultSet.getString("text_state")),
-                        resultSet.getString("text")
-                ),
-                toStringField(
-                        SlackNotificationOutboxFieldState.valueOf(resultSet.getString("blocks_json_state")),
-                        resultSet.getString("blocks_json")
-                ),
-                toStringField(
-                        SlackNotificationOutboxFieldState.valueOf(resultSet.getString("fallback_text_state")),
-                        resultSet.getString("fallback_text")
-                ),
-                SlackNotificationOutboxStatus.valueOf(resultSet.getString("status")),
-                resultSet.getInt("processing_attempt"),
-                toProcessingLease(resultSet),
-                toSentTime(resultSet),
-                toFailedTime(resultSet),
-                toFailure(resultSet)
-        );
+        String failureReason = resultSet.getString("failure_reason");
+        String failureType = resultSet.getString("failure_type");
+        SlackInteractionFailureType actualFailureType = null;
+        if (failureReason == null && failureType == null) {
+            actualFailureType = null;
+        } else if (failureReason == null || failureType == null) {
+            throw new IllegalStateException("failure_reason과 failure_type은 항상 함께 세팅되어야 합니다.");
+        } else {
+            actualFailureType = SlackInteractionFailureType.valueOf(failureType);
+        }
+
+        Timestamp processingStartedAt = resultSet.getTimestamp("processing_started_at");
+        Timestamp sentAt = resultSet.getTimestamp("sent_at");
+        Timestamp failedAt = resultSet.getTimestamp("failed_at");
+        Instant actualProcessingStartedAt = null;
+        if (processingStartedAt != null) {
+            actualProcessingStartedAt = processingStartedAt.toInstant();
+        }
+        Instant actualSentAt = null;
+        if (sentAt != null) {
+            actualSentAt = sentAt.toInstant();
+        }
+        Instant actualFailedAt = null;
+        if (failedAt != null) {
+            actualFailedAt = failedAt.toInstant();
+        }
+        SlackNotificationOutboxRow row = SlackNotificationOutboxRow.builder()
+                                                                   .id(resultSet.getLong("id"))
+                                                                   .messageType(
+                                                                           SlackNotificationOutboxMessageType.valueOf(
+                                                                                   resultSet.getString("message_type")
+                                                                           )
+                                                                   )
+                                                                   .idempotencyKey(resultSet.getString("idempotency_key"))
+                                                                   .teamId(resultSet.getString("team_id"))
+                                                                   .channelId(resultSet.getString("channel_id"))
+                                                                   .userIdState(
+                                                                           SlackNotificationOutboxFieldState.valueOf(
+                                                                                   resultSet.getString("user_id_state")
+                                                                           )
+                                                                   )
+                                                                   .userId(resultSet.getString("user_id"))
+                                                                   .textState(
+                                                                           SlackNotificationOutboxFieldState.valueOf(
+                                                                                   resultSet.getString("text_state")
+                                                                           )
+                                                                   )
+                                                                   .text(resultSet.getString("text"))
+                                                                   .blocksJsonState(
+                                                                           SlackNotificationOutboxFieldState.valueOf(
+                                                                                   resultSet.getString("blocks_json_state")
+                                                                           )
+                                                                   )
+                                                                   .blocksJson(resultSet.getString("blocks_json"))
+                                                                   .fallbackTextState(
+                                                                           SlackNotificationOutboxFieldState.valueOf(
+                                                                                   resultSet.getString("fallback_text_state")
+                                                                           )
+                                                                   )
+                                                                   .fallbackText(resultSet.getString("fallback_text"))
+                                                                   .status(SlackNotificationOutboxStatus.valueOf(resultSet.getString("status")))
+                                                                   .processingAttempt(resultSet.getInt("processing_attempt"))
+                                                                   .processingLeaseState(
+                                                                           BoxProcessingLeaseState.valueOf(
+                                                                                   resultSet.getString("processing_lease_state")
+                                                                           )
+                                                                   )
+                                                                   .processingStartedAt(actualProcessingStartedAt)
+                                                                   .sentTimeState(
+                                                                           BoxEventTimeState.valueOf(
+                                                                                   resultSet.getString("sent_time_state")
+                                                                           )
+                                                                   )
+                                                                   .sentAt(actualSentAt)
+                                                                   .failedTimeState(
+                                                                           BoxEventTimeState.valueOf(
+                                                                                   resultSet.getString("failed_time_state")
+                                                                           )
+                                                                   )
+                                                                   .failedAt(actualFailedAt)
+                                                                   .failureState(
+                                                                           BoxFailureState.valueOf(
+                                                                                   resultSet.getString("failure_state")
+                                                                           )
+                                                                   )
+                                                                   .failureReason(failureReason)
+                                                                   .failureType(actualFailureType)
+                                                                   .build();
+
+        return row.toDomain();
     }
 
     private SlackNotificationOutboxHistory mapHistory(ResultSet resultSet) throws SQLException {
-        return SlackNotificationOutboxHistory.rehydrate(
-                resultSet.getLong("id"),
-                resultSet.getLong("outbox_id"),
-                resultSet.getInt("processing_attempt"),
-                SlackNotificationOutboxStatus.valueOf(resultSet.getString("status")),
-                resultSet.getTimestamp("completed_at").toInstant(),
-                toFailure(resultSet)
-        );
-    }
-
-    private SlackNotificationOutboxStringField toStringField(
-            SlackNotificationOutboxFieldState state,
-            String value
-    ) {
-        if (state == SlackNotificationOutboxFieldState.ABSENT) {
-            return SlackNotificationOutboxStringField.absent();
-        }
-
-        return SlackNotificationOutboxStringField.present(value);
-    }
-
-    private BoxProcessingLease toProcessingLease(ResultSet resultSet) throws SQLException {
-        if (resultSet.getTimestamp("processing_started_at") == null) {
-            return BoxProcessingLease.idle();
-        }
-
-        return BoxProcessingLease.claimed(resultSet.getTimestamp("processing_started_at").toInstant());
-    }
-
-    private BoxEventTime toSentTime(ResultSet resultSet) throws SQLException {
-        if (resultSet.getTimestamp("sent_at") == null) {
-            return BoxEventTime.absent();
-        }
-
-        return BoxEventTime.present(resultSet.getTimestamp("sent_at").toInstant());
-    }
-
-    private BoxEventTime toFailedTime(ResultSet resultSet) throws SQLException {
-        if (resultSet.getTimestamp("failed_at") == null) {
-            return BoxEventTime.absent();
-        }
-
-        return BoxEventTime.present(resultSet.getTimestamp("failed_at").toInstant());
-    }
-
-    private BoxFailureSnapshot<SlackInteractionFailureType> toFailure(ResultSet resultSet) throws SQLException {
         String failureReason = resultSet.getString("failure_reason");
         String failureType = resultSet.getString("failure_type");
-
+        SlackInteractionFailureType actualFailureType = null;
         if (failureReason == null && failureType == null) {
-            return BoxFailureSnapshot.absent();
-        }
-        if (failureReason == null || failureType == null) {
+            actualFailureType = null;
+        } else if (failureReason == null || failureType == null) {
             throw new IllegalStateException("failure_reason과 failure_type은 항상 함께 세팅되어야 합니다.");
+        } else {
+            actualFailureType = SlackInteractionFailureType.valueOf(failureType);
         }
 
-        return BoxFailureSnapshot.present(
-                failureReason,
-                SlackInteractionFailureType.valueOf(failureType)
-        );
+        SlackNotificationOutboxHistoryRow row = SlackNotificationOutboxHistoryRow.builder()
+                                                                                 .id(resultSet.getLong("id"))
+                                                                                 .outboxId(resultSet.getLong("outbox_id"))
+                                                                                 .processingAttempt(resultSet.getInt("processing_attempt"))
+                                                                                 .status(
+                                                                                         SlackNotificationOutboxStatus.valueOf(
+                                                                                                 resultSet.getString("status")
+                                                                                         )
+                                                                                 )
+                                                                                 .completedAt(resultSet.getTimestamp("completed_at").toInstant())
+                                                                                 .failureState(
+                                                                                         BoxFailureState.valueOf(
+                                                                                                 resultSet.getString("failure_state")
+                                                                                         )
+                                                                                 )
+                                                                                 .failureReason(failureReason)
+                                                                                 .failureType(actualFailureType)
+                                                                                 .build();
+
+        return row.toDomain();
     }
 }
